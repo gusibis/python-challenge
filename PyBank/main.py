254ab848e07da0caa2f45c1ef6232b62485df952
gusibis <gus1ibis@gmail.com>
2023-03-07 17:31:35 -0700

    Fixed some of the code to follow object oriented model

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,29 +3,23 @@ import os
 import traceback
 
 class OpenParseCSV():
-    def __init__(self, sourcePathFile = None, destPathFile = None, sourcePath = None, destPath = None):
-        self.sourcePathFile = sourcePathFile 
-        self.destPathFile = destPathFile
-        self.sourcePath = sourcePath
-        self.destPath = destPath
-        try:
-            self.analizeFile()
-        except:
-            print("Something went wrong. Verify the source file exists in", self.sourcePath, "and try running the script again") #Another printing methos that automatically provides spaces
-            traceback.print_exc()
-            return
-        try:
-            self.printAndOutputFile()                                                                                                                                     
-        except:
-            print(f"Something went wrong. Verify the destination location still exists, inspect {self.destPath} , perhaps another application deleted this location.") 
-            traceback.print_exc()
-            return
+    def __init__(self):
+        self.sourcePath=(os.getcwd() + "/Resources/") # get local source path
+        self.sourcePathFile = os.path.abspath(os.getcwd() + "/Resources/budget_data.csv") # Get local source path file name
+        self.destPath = os.path.abspath(os.getcwd() + "/analysis/") # assign destination path
+        self.destPathFile=(self.destPath + "/Financial Analysis.txt") # assign destination file name
+        if not os.path.exists(self.destPath): # Check if destination location exists, else create it, but should exist if the challenge instructions were followed. 
+            os.makedirs(self.destPath)
+    
+        if not os.path.isfile(self.sourcePathFile): # if the sourcepath does not exist end the script, path should exist if instructions were followed. 
+            print("SOURCE FILE DOES NOT EXIST. MAKE SURE THIS PATH EXISTS: {self.sourcePathFile}")
+            self.endScript()
 
     def analizeFile(self):
-        dictOfChanges = dict()  # create a dictionary, 2 lists and a counter for the total number of months
-        listOfProfitLoses = list()
-        changeList = list()
-        totalNumberOfMonths = 0
+        self.dictOfChanges = dict()  # create a dictionary, 2 lists and a counter for the total number of months
+        self.listOfProfitLoses = list()
+        self.changeList = list()
+        self.totalNumberOfMonths = 0
         with open(self.sourcePathFile,"r") as csvfile: 
             contents = csv.reader(csvfile)  # Read the CSV file
             next(csvfile) # This is the tile row, skipping it.
@@ -35,24 +29,24 @@ class OpenParseCSV():
             for row in contents:  #iterate through hte CSV file to collect data and make calculations, content is now is missing the title and the first value.
                 profitLoses = int(row[1])
                 changeCalc = profitLoses - oldValue
-                changeList.append(changeCalc)
-                dictOfChanges.update({changeCalc : row[0]})  #build a dictionary to get the highest and lowest change in profit at the end. 
-                totalNumberOfMonths += 1
-                listOfProfitLoses.append(int(profitLoses))
+                self.changeList.append(changeCalc)
+                self.dictOfChanges.update({changeCalc : row[0]})  #build a dictionary to get the highest and lowest change in profit at the end. 
+                self.totalNumberOfMonths += 1
+                self.listOfProfitLoses.append(int(profitLoses))
                 oldValue = profitLoses
 
-        self.totalNumberOfMonths = str(totalNumberOfMonths  + 1) # total months plus one since the first row was removed at the begining by the next function.  
-        self.totalAmountText = "$" + str(sum(listOfProfitLoses) + fValue) # sum of values plus the first value that was also removed by the next function
-        self.averageChangeText = "$" + str(round(sum(changeList) / len(changeList), 2))
-        self.greatestIncrease = max(changeList) #extracting the greatest increase and decrease values and dates from the dictionary built in the for loop. 
-        self.greatestDecrease = min(changeList)
-        self.gIncreaseDate = dictOfChanges[self.greatestIncrease] 
-        self.gDecreaseDate = dictOfChanges[self.greatestDecrease]
-        self.greatestIncreaseText = self.gIncreaseDate + " ($" + str(max(changeList)) + ")"
-        self.greatestDecreaseText = self.gDecreaseDate + " ($" + str(min(changeList)) + ")"
+        self.totalNumberOfMonths = str(self.totalNumberOfMonths  + 1) # total months plus one since the first row was removed at the begining by the next function.  
+        self.totalAmountText = "$" + str(sum(self.listOfProfitLoses) + fValue) # sum of values plus the first value that was also removed by the next function
+        self.averageChangeText = "$" + str(round(sum(self.changeList) / len(self.changeList), 2))
+        self.greatestIncrease = max(self.changeList) #extracting the greatest increase and decrease values and dates from the dictionary built in the for loop. 
+        self.greatestDecrease = min(self.changeList)
+        self.gIncreaseDate = self.dictOfChanges[self.greatestIncrease] 
+        self.gDecreaseDate = self.dictOfChanges[self.greatestDecrease]
+        self.greatestIncreaseText = self.gIncreaseDate + " ($" + str(max(self.changeList)) + ")"
+        self.greatestDecreaseText = self.gDecreaseDate + " ($" + str(min(self.changeList)) + ")"
 
     def printAndOutputFile(self):
-        printList = [   # concoct the list to print and to make the file 
+        self.printList = [   # concoct the list to print and to make the file 
             "Financial Analysis",
             "",
             "----------------------------",
@@ -69,30 +63,32 @@ class OpenParseCSV():
             "",
         ]
 
-        for statement in printList: print(statement)
+        for statement in self.printList: print(statement)
 
         print(f'CREATING FILE "Financial Analysis.txt" IN LOCATION {self.destPath}') 
 
         with open(self.destPathFile, "w") as txtFile:
-            for statement in printList:
+            for statement in self.printList:
                 txtFile.write("%s\n" % statement) # write each item on a new line
    
         print("FILE CREATED.")  
         print("EXITING SCRIPT.") 
 
-    def endScript():
+    def endScript(self):
         return  
 
 
 if __name__ == "__main__":
-    sourcePathFile = os.path.abspath(os.getcwd() + "/Resources/budget_data.csv") # get source path 
-    
-    if not os.path.isfile(sourcePathFile):
-        print("SOURCE FILE DOES NOT EXIST. MAKE SURE THIS PATH EXISTS: {sourcePathFile}")
-        OpenParseCSV.endScript()
-
-    destPath = os.path.abspath(os.getcwd() + "/analysis/")
-    if not os.path.exists(destPath): # Check if destination location exists, else create it, but if the challenge documentation was followed it should already exist. 
-        os.makedirs(destPath)
-
-    OpenParseCSV(sourcePathFile, destPathFile=(destPath + "/Financial Analysis.txt"), sourcePath=(os.getcwd() + "/Resources/"), destPath = destPath)
+    openParseCSV = OpenParseCSV() #instantiate the class. 
+    try:
+        openParseCSV.analizeFile()
+    except:
+        print("Something went wrong. Verify the source file exists in", openParseCSV.sourcePath, "and try running the script again") #Another printing methos that automatically provides spaces
+        traceback.print_exc()
+        openParseCSV.endScript()
+    try:
+        openParseCSV.printAndOutputFile()                                                                                                                                     
+    except:
+        print(f"Something went wrong. Verify the destination location still exists, inspect {openParseCSV.destPath} , perhaps another application deleted this location.") 
+        traceback.print_exc()
+        openParseCSV.endScript()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,53 +3,43 @@ import os
 import traceback
 
 class OpenParseCSV():
-    def __init__(self, sourcePathFile = None, destPathFile = None, sourcePath = None, destPath = None):
-        self.sourcePathFile = sourcePathFile 
-        self.destPathFile = destPathFile
-        self.sourcePath = sourcePath
-        self.destPath = destPath
-        try:
-            self.parseFile()
-        except:
-            print("Something went wrong. Verify the source file exists in", self.sourcePath, "and try running the script again") #Another printing methos that automatically provides spaces
-            traceback.print_exc()
-            return
-        try:
-            self.printAndOutputFile()                                                                                                                                     
-        except:
-            print(f"Something went wrong. Verify the destination location still exists, inspect {self.destPath} , perhaps another application deleted this location.") 
-            traceback.print_exc()
-            return
-
+    def __init__(self):
+        self.sourcePath=(os.getcwd() + "/Resources/") # get local source path
+        self.sourcePathFile = os.path.abspath(os.getcwd() + "/Resources/election_data.csv")  # Get local source path file name
+        self.destPath = os.path.abspath(os.getcwd() + "/analysis/") # assign destination path
+        self.destPathFile=(self.destPath + "/Election Results.txt") # assign destination file name
+        if not os.path.exists(self.destPath): # Check if destination location exists, else create it, but should exist if the challenge instructions were followed. 
+            os.makedirs(self.destPath)
+        
     def parseFile(self):
-        ballotId = list()
-        candidate = list()
+        self.ballotId = list()
+        self.candidate = list()
         with open(self.sourcePathFile,"r") as csvfile: 
             content = csv.reader(csvfile)  #Read the CSV file
             # titles = (next(csvfile).replace("\n","")).split(",") # This is the tile row, comes in this format 'Ballot ID,County,Candidate\n' Not used, so I commented it out. 
             for row in content: 
                 if "Ballot ID" in row: continue # skip the titles row
-                ballotId.append(row[0]) 
-                candidate.append((row[2]).replace("\n",""))
+                self.ballotId.append(row[0]) 
+                self.candidate.append((row[2]).replace("\n",""))
 
-        candidatesDict = {}
-        for cand in set(candidate): # set is a data type used to store multiple items in a single variable https://realpython.com/python-sets/  https://www.w3schools.com/python/python_sets.asp
+        self.candidatesDict = {}
+        for cand in set(self.candidate): # set is a data type used to store multiple items in a single variable https://realpython.com/python-sets/  https://www.w3schools.com/python/python_sets.asp
             if 'CANDIDATE' in cand.upper(): continue #Skip the title   
-            candidatesDict[cand] = candidate.count(cand) # count each candidates instances from the set and put them in the candidatesDict dictionary
+            self.self.candidatesDict[cand] = self.candidate.count(cand) # count each candidates instances from the set and put them in the candidatesDict dictionary
         
-        self.totalVotes = len(ballotId) # total votes can also be calculated 
+        self.totalVotes = len(self.ballotId) # total votes can also be calculated 
         # self.totalVotes = sum(candidatesDict.values()) # Could have gotten the total votes like this also
-        self.firstPlaceName = max(candidatesDict, key = candidatesDict.get) # Get the candidate with the higher number of votes
-        self.firstPlaceText = self.firstPlaceName + ": " + str(round(((max(candidatesDict.values()) / self.totalVotes)*100),3)) + "% (" + str(max(candidatesDict.values())) + ")"
+        self.firstPlaceName = max(self.candidatesDict, key = self.candidatesDict.get) # Get the candidate with the higher number of votes
+        self.firstPlaceText = self.firstPlaceName + ": " + str(round(((max(self.candidatesDict.values()) / self.totalVotes)*100),3)) + "% (" + str(max(self.candidatesDict.values())) + ")"
         
         self.listOfNonWinners = [] # start a new list to put the other candidates
-        for key, value in candidatesDict.items(): 
+        for key, value in self.candidatesDict.items(): 
             if key in self.firstPlaceName: continue # Skipt the winner we already collected above
-            nextCandidate = key + ": " + str(round((value/self.totalVotes)*100, 3)) + "% (" + str(value) + ")"
-            self.listOfNonWinners.append(nextCandidate)
+            nextCandidateText = key + ": " + str(round((value/self.totalVotes)*100, 3)) + "% (" + str(value) + ")"
+            self.listOfNonWinners.append(nextCandidateText)
    
     def printAndOutputFile(self):
-        printList = [   # concoct the list to print and to make the file 
+        self.printList = [   # concoct the list to print and to make the file 
             "Election Results",
             "",
             "-------------------------",
@@ -71,37 +61,39 @@ class OpenParseCSV():
         ]
          
         for otherCandidate in self.listOfNonWinners:
-            winnerIndex = printList.index(self.firstPlaceText) # find the index of our winner in the list.
+            winnerIndex = self.printList.index(self.firstPlaceText) # find the index of our winner in the list.
             if "Charles Casper Stockham" in otherCandidate: # added this conditional to make it look exactly as in the challenge assigment, I am after 100%. 
                 inserIndex = winnerIndex - 1
             else:
                 inserIndex = winnerIndex + 2
   
-            printList.insert(inserIndex, otherCandidate) #insert other candidate in the correct place to match challenge text file. 
+            self.printList.insert(inserIndex, otherCandidate) #insert other candidate in the correct place to match challenge text file. 
 
-        for statement in printList: print(statement)
+        for statement in self.printList: print(statement)
 
         print(f'CREATING "Election Results.txt" FILE IN LOCATION {self.destPath}') 
 
         with open(self.destPathFile, "w") as txtFile:
-            for statement in printList:
+            for statement in self.printList:
                 txtFile.write("%s\n" % statement) # write each item on a new line
    
         print("FILE CREATED.")  
         print("EXITING SCRIPT.") 
 
-    def endScript():
+    def endScript(self):
         return  
 
 if __name__ == "__main__":
-    sourcePathFile = os.path.abspath(os.getcwd() + "/Resources/election_data.csv") # get source path, abspath used in case the evaluator uses Linux. 
-    
-    if not os.path.isfile(sourcePathFile):
-        print("SOURCE FILE DOES NOT EXIST. MAKE SURE THIS PATH EXISTS: {sourcePathFile}")
-        OpenParseCSV.endScript()
-
-    destPath = os.path.abspath(os.getcwd() + "/analysis/")
-    if not os.path.exists(destPath): # Check if destination location exists, else create it, but should exist if the challenge instructions were followed. 
-        os.makedirs(destPath)
-
-    OpenParseCSV(sourcePathFile, destPathFile=(destPath + "/Election Results.txt"), sourcePath=(os.getcwd() + "/Resources/"), destPath = destPath)
+    openParseCSV = OpenParseCSV() #instantiate the class. 
+    try:
+        openParseCSV.parseFile()
+    except:
+        print("Something went wrong. Verify the source file exists in", openParseCSV.sourcePath, "and try running the script again") #Another printing methos that automatically provides spaces
+        traceback.print_exc()
+        openParseCSV.endScript()
+    try:
+        openParseCSV.printAndOutputFile()                                                                                                                                     
+    except:
+        print(f"Something went wrong. Verify the destination location still exists, inspect {openParseCSV.destPath} , perhaps another application deleted this location.") 
+        traceback.print_exc()
+        openParseCSV.endScript()
